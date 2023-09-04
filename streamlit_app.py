267c0407e063da2f785bef3148c6ff52ebcd7226
ViniciosBK <71196927+ViniciosBK@users.noreply.github.com>
2023-09-04 20:10:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,61 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.title("Software de Backtesting")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+@st.cache(allow_output_mutation=True)
+def load_base():
+    url = "https://github.com/futpythontrader/YouTube/blob/main/Base_de_Dados/futpythontraderpunter.csv?raw=true"
+    data_jogos = pd.read_csv(url)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    return data_jogos
 
-    points_per_turn = total_points / num_turns
+df = load_base()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+df.loc[(df['FT_Goals_H'] >  df['FT_Goals_A']), 'Profit'] = df['FT_Odd_H'] - 1
+df.loc[(df['FT_Goals_H'] <= df['FT_Goals_A']), 'Profit'] = -1
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Caixa de texto para filtrar Odd Home
+min_H = st.sidebar.number_input("FT_Odd_H", min_value=1.01, max_value=10.0, value=1.01)
+max_H = st.sidebar.number_input("FT_Odd_H", min_value=1.01, max_value=10.0, value=10.0)
+
+# Caixa de texto para filtrar Odd Draw
+min_D = st.sidebar.number_input("FT_Odd_D", min_value=1.01, max_value=10.0, value=1.01)
+max_D = st.sidebar.number_input("FT_Odd_D", min_value=1.01, max_value=10.0, value=10.0)
+
+# Caixa de texto para filtrar Odd Away
+min_A = st.sidebar.number_input("FT_Odd_A", min_value=1.01, max_value=10.0, value=1.01)
+max_A = st.sidebar.number_input("FT_Odd_A", min_value=1.01, max_value=10.0, value=10.0)
+
+# Caixa de texto para filtrar Odd Over 25
+min_Over25 = st.sidebar.number_input("FT_Odd_Over25", min_value=1.01, max_value=10.0, value=1.01)
+max_Over25 = st.sidebar.number_input("FT_Odd_Over25", min_value=1.01, max_value=10.0, value=10.0)
+
+# Caixa de texto para filtrar Odd BTTS
+min_BTTS = st.sidebar.number_input("FT_Odd_BTTS_Yes", min_value=1.01, max_value=10.0, value=1.01)
+max_BTTS = st.sidebar.number_input("FT_Odd_BTTS_Yes", min_value=1.01, max_value=10.0, value=10.0)
+
+# Filtre o dataframe pelos valores mínimos e máximos de cada coluna
+df_filtrado = df[(df['FT_Odd_H'] >= min_H) & (df['FT_Odd_H'] <= max_H) &
+                 (df['FT_Odd_D'] >= min_D) & (df['FT_Odd_D'] <= max_D) &
+                 (df['FT_Odd_A'] >= min_A) & (df['FT_Odd_A'] <= max_A) &
+                 (df['FT_Odd_Over25'] >= min_Over25) & (df['FT_Odd_Over25'] <= max_Over25) &
+                 (df['FT_Odd_BTTS_Yes'] >= min_BTTS) & (df['FT_Odd_BTTS_Yes'] <= max_BTTS)]
+
+# Crie uma nova coluna no dataframe filtrado com o profit acumulado
+df_filtrado['Profit_acu'] = df_filtrado.Profit.cumsum()
+df_filtrado = df_filtrado.dropna()
+df_filtrado = df_filtrado.reset_index(drop=True)
+df_filtrado.index += 1
+profit = round(df_filtrado.Profit_acu.tail(1).item(),2)
+ROI = round((df_filtrado.Profit_acu.tail(1)/len(df_filtrado)*100).item(),2)
+df_filtrado.Profit_acu.plot(title="Back Home", xlabel='Entradas', ylabel='Stakes')
+print("Profit:",profit,"stakes em", len(df_filtrado),"jogos")
+print("ROI:",ROI,"%")
+
+# Plote o gráfico com o profit acumulado do dataframe filtrado
+plt.plot(df_filtrado['Profit_acu'])
+st.pyplot()
+st.set_option('deprecation.showPyplotGlobalUse', False)
